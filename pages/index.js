952ef19ba09509442c20dbd9940d3eacfcb679d9
Moh Amilin <amilinmoh99@gmail.com>
2023-10-29 14:16:33 +0700

    update page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { Box, Container, Dialog, Grid } from "@mui/material";
 
 import Layout from "@/layouts/Layout";
@@ -6,11 +6,14 @@ import SEO from "@/components/SEO";
 import CarouselSection from "@/layouts/LandingPage/CarouselSection";
 import Feature from "@/layouts/LandingPage/Feature";
 import TopCampuses from "@/layouts/LandingPage/TopCampuses";
+import Scholarships from "@/layouts/LandingPage/Scholarships";
+import apiHomepage from "@/utils/__api__/homepage";
 
-const Home = () => {
+const Home = (props) => {
   const [dialogOpen, setDialogOpen] = useState(false);
   const toggleDialog = () => setDialogOpen(!dialogOpen);
 
+  console.log(props.homepage)
   return (
     <>
       <Layout>
@@ -24,7 +27,10 @@ const Home = () => {
               <Feature />
             </Grid>
             <Grid sx={{mt:2, mb:2}}>
-              <TopCampuses />
+              <TopCampuses campuses={props.homepage.campuses.result.data} />
+            </Grid>
+            <Grid sx={{mt:2, mb:2}}>
+              <Scholarships scholarships={props.homepage.scholarships.result.data} />
             </Grid>
           </Container>
         </Box>
@@ -35,3 +41,18 @@ const Home = () => {
 
 Home.layout = "Blank";
 export default Home;
+
+export const getStaticProps = async () => {
+  const page = 1;
+  const per_page = 12
+
+
+  const params = {page, per_page}
+  const homepage = await apiHomepage.getHomepage(params)
+
+  return {
+    props : {
+      homepage
+    }
+  }
+}
